bf29f2b4f88c1f1c5b8969a0827e474dcdc59441
Joe Ligman <jwligman@us.ibm.com>
2017-07-10 17:08:25 -0400

    move working examples to the swift playground

--- examples/swift/SwitExamplesPlayground.playground/Contents.swift
+++ examples/swift/SwitExamplesPlayground.playground/Contents.swift
@@ -8,12 +8,43 @@ import qiskit
 var testurl = "https://quantumexperience.ng.bluemix.net/api/"
 var apitoken = "NONE"
 
-do {
-    let qconf = try Qconfig(APItoken: apitoken, url: testurl)
-    try RippleAdd.rippleAdd(qConfig: qconf)
-} catch {
-    debugPrint(error.localizedDescription)
+func runDataMove() {
+    do {
+        let qconf = try Qconfig(APItoken: apitoken, url: testurl)
+        try DataMove.dataMove(qConfig: qconf)
+    } catch {
+        debugPrint(error.localizedDescription)
+    }
+}
+
+func runGHZ() {
+    do {
+        let qconf = try Qconfig(APItoken: apitoken, url: testurl)
+        try GHZ.ghz(qConfig: qconf)
+    } catch {
+        debugPrint(error.localizedDescription)
+    }
+}
+
+func runMultiple() {
+    do {
+        let qconf = try Qconfig(APItoken: apitoken, url: testurl)
+        try Multiple.multiple(qConfig: qconf)
+    } catch {
+        debugPrint(error.localizedDescription)
+    }
 }
 
 
+func runRippleExample() {
+    do {
+        let qconf = try Qconfig(APItoken: apitoken, url: testurl)
+        try RippleAdd.rippleAdd(qConfig: qconf)
+    } catch {
+        debugPrint(error.localizedDescription)
+    }
+}
+
+
+//TODO run your example here
 PlaygroundPage.current.needsIndefiniteExecution = true
